68f913e04614d1e5feaf2c443b87daa8b52b7a76
Miguel <mjrodrigueza@ufpso.edu.co>
2025-10-01 22:41:19 -0500

    PM-18 Crear Servicio

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../assets/Sidebar.css";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <aside className="sidebar">
       <div className="sidebar-logo">🧠 Salud Mental</div>
       <ul className="sidebar-links">
         <li><a href="#frases">Estudiantes</a></li>
         <li><a href="#tips">Psicoorientadores</a></li>
-        <li><a href="#informacion">Servicios</a></li>
+        <li>
+          <a 
+            href="#servicios" 
+            onClick={(e) => {
+              e.preventDefault();
+              navigate('/servicios');
+            }}
+          >
+            Servicios
+          </a>
+        </li>
       </ul>
     </aside>
   );
