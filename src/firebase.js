4ca55cd68e0fb0c42fef57eefa043f50f37f4fb2
Miguel <mjrodrigueza@ufpso.edu.co>
2025-11-08 10:35:03 -0500

    Se agrego la funcion de restablecer contraseña

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signOut, sendPasswordResetEmail, confirmPasswordReset, verifyPasswordResetCode } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signOut, sendPasswordResetEmail, confirmPasswordReset, verifyPasswordResetCode, fetchSignInMethodsForEmail, linkWithCredential } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -31,4 +31,4 @@ const FacebookProvider = new FacebookAuthProvider();
 //conexión a db
 const db = getFirestore(app);
 //Explotar variables para consumo del proyectpo
-export { auth, GoogleProvider, GithubProvider, FacebookProvider, db, signOut, sendPasswordResetEmail, confirmPasswordReset, verifyPasswordResetCode };
+export { auth, GoogleProvider, GithubProvider, FacebookProvider, db, signOut, sendPasswordResetEmail, confirmPasswordReset, verifyPasswordResetCode, fetchSignInMethodsForEmail, linkWithCredential };
